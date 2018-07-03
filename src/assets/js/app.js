898f09624dc4ff040195f8432af8ec601f52d425
Desarrollo Front-End <desarrollofront-end@Francisco-Cardumen.local>
2018-07-03 09:33:43 -0400

    se genera primeros diseños

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,36 @@
+require("velocity-animate");
 require("../scss/app.scss");
+
+
+$( document ).ready(function() {
+
+	var McButton = $("[data=hamburger-menu]");
+	var McBar1 = McButton.find("b:nth-child(1)");
+	var McBar2 = McButton.find("b:nth-child(2)");
+	var McBar3 = McButton.find("b:nth-child(3)");
+
+
+
+	McButton.click( function() {
+	  $(this).stop().toggleClass("active");
+	  
+	  if (McButton.hasClass("active")) {
+	    McBar1.velocity({ top: "50%" }, {duration: 200, easing: "swing"});
+	    McBar3.velocity({ top: "50%" }, {duration: 200, easing: "swing"})
+	    			.velocity({rotateZ:"90deg"}, {duration: 800, delay: 200, easing: [500,20] });
+	    McButton.velocity({rotateZ:"135deg"}, {duration: 800, delay: 200, easing: [500,20] });
+	  } else {
+	    McButton.velocity("reverse");
+			McBar3.velocity({rotateZ:"0deg"}, {duration: 800, easing: [500,20] })
+	    			.velocity({ top: "100%" }, {duration: 200, easing: "swing"});
+	  	McBar1.velocity("reverse", {delay: 800});
+	  }
+	  $(".menu").fadeIn();
+	});
+
+
+});
+
 
 if (module.hot) {
   module.hot.accept();
